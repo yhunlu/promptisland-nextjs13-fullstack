dd2f8ccead797d357b741d4138253a166ca37990
Yahya Unlu <yhunlu87@gmail.com>
2023-06-04 21:25:37 +0300

    added component structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,21 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Find & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+          PromptIsland is a platform who looking 
+          for AI prompts about CHATGPT, StableDiffusion or etc.
+      </p>
+      <p className="text-center font-bold">
+        #SharingCaring
+      </p>
+      {/* Feed */}
+    </section>
   )
 }
 
